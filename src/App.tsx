2d6170111e364cc13db6ada21593877e2b582f4e
AustrianMethod <ivanov.nv.spb@gmail.com>
2024-12-07 13:10:25 +0300

    routing with react-router-dom and splitting bundle to small chunks with React lazy and Suspense component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import {Counter} from './components/Counter';
+import { Suspense } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
 import './index.scss'
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
 
 const App = () => {
     return (
         <div className='app'>
-            sdasdasdasdasaSSS
-            <Counter/> 
+            <Link to={'/'}>Главная страница</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                </Routes> 
+            </Suspense>
         </div>
     )
 }
 
-export default App;
+export default App; 
--- src/test.ts
+++
@@ -1,5 +0,0 @@
-export function someFn(arg: number): string {
-   
-   console.log('RANDOM FUNCTION dfsdfs 222');
-   return 'lols'
-}
